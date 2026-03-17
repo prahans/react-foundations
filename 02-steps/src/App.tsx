948dbefba5cc5572eca35f79470aef306025f036
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-03-17 18:12:21 +0545

    using children props

--- 02-steps/src/App.tsx
+++ 02-steps/src/App.tsx
@@ -40,14 +40,36 @@ function Steps() {
         <div className={step >= 2 ? 'active' : ""}>2</div>
         <div className={step >= 3 ? 'active' : ""}>3</div>
       </div>
-      <p className="message">Step {step}: {messages[step -1]}</p>
+      <StepMessage step={step} >{messages[step -1]}</StepMessage>
       <div className="buttons">
-        <button style={{backgroundColor: "#7950f2", color: "#fff"}} onClick={handlePrevious} >Previous</button>
-        <button style={{backgroundColor: "#7950f2", color: "#fff"}} onClick={handleNext}>Next</button>
+        <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}> <span>👈</span> Previous</Button>
+        <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}> Next <span>👉</span></Button>
       </div>
     </div>
     )}
     </div>
+  )
+}
+
+function StepMessage({step, children}: {step: number; children: React.ReactNode;}){
+  return (<div className="message">
+    <h3>Step {step} </h3> 
+    {children}
+   </div>)
+}
+
+type ButtonProps = {
+  textColor: string;
+  bgColor: string;
+  onClick: () => void;
+  children: React.ReactNode;
+}
+
+function Button({textColor, bgColor, onClick, children}: ButtonProps){
+  return (
+    <button style={{backgroundColor: bgColor, color: textColor}} onClick={onClick}>
+      {children}
+    </button>
   )
 }
 
