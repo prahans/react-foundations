27133293029d34fa86bbe8e6d318b52eddf3fdd5
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-03-17 08:46:59 +0545

    adding clear button

---
+++ flashCard/src/App.tsx
@@ -0,0 +1,79 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+  function handleInc(){
+    setCount(count + 1);
+  }
+  function handleDec(){
+    setCount(count - 1);
+  }
+
+
+  return (<>
+    <FlashCards/>
+    <div style={{display: 'flex'}}>
+    <button onClick={handleInc}>increase Count</button>
+    <p>{count}</p>
+    <button onClick={handleDec}>decrease Count</button>
+    </div>
+    </>
+  )
+}
+
+const questions = [
+  {
+    id: 3457,
+    question: "What language is React based on?",
+    answer: "JavaScript"
+  },
+  {
+    id: 7336,
+    question: "What are the building blocks of React apps?",
+    answer: "Components"
+  },
+  {
+    id: 8832,
+    question: "What's the name of the syntax we use to describe a UI in React?",
+    answer: "JSX"
+  },
+  {
+    id: 1297,
+    question: "How to pass data from parent to child components?",
+    answer: "Props"
+  },
+  {
+    id: 9103,
+    question: "How to give components memory?",
+    answer: "useState hook"
+  },
+  {
+    id: 2002,
+    question:
+      "What do we call an input element that is completely synchronised with state?",
+    answer: "Controlled element"
+  }
+];
+
+function FlashCards() {
+  const [selectedId, setSelectedId] = useState(0);
+  function handleClick(id: number){
+    setSelectedId(id !== selectedId ? id : 0);
+  }
+  return <div className='flashcards'>
+    {
+      questions.map(el => 
+        <div
+        key={el.id}
+        onClick={() => handleClick(el.id)}
+        className={el.id === selectedId ? "selected" : ""}
+        >
+        <p>{el.id === selectedId ? el.answer : el.question}</p>
+        </div>
+      )
+    }
+  </div>;
+}
+
+export default App
