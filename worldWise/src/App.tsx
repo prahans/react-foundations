2c2d8b1531f8bf1970373fab9e0cdaeab76fe9b5
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-04-24 09:01:40 +0545

    using react route

--- worldWise/src/App.tsx
+++ worldWise/src/App.tsx
@@ -1,11 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Product from './pages/Product'
+import Homepage from './pages/Homepage'
+import Pricing from './pages/Pricing'
+import PageNotFound from './pages/PageNotFount'
 
 function App() {
 
   return (
-    <div>
-      worldWise
-    </div>
+    <BrowserRouter>
+      <Routes>
+         <Route path='/' element={<Homepage />} />
+         <Route path='Product' element={<Product />} />
+         <Route path='Pricing' element={<Pricing />} />
+         <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
