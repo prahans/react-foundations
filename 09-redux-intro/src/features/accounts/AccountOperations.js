123bb5b0eb11c32f91f477991610bf00faac7328
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-07-14 19:42:41 +0545

    using redux

---
+++ 09-redux-intro/src/features/accounts/AccountOperations.js
@@ -0,0 +1,115 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { deposit } from "./accountSlice";
+import { withdraw } from "./accountSlice";
+import { requestLoan } from "./accountSlice";
+import { payLoan } from "./accountSlice";
+
+function AccountOperations() {
+  const [depositAmount, setDepositAmount] = useState("");
+  const [withdrawalAmount, setWithdrawalAmount] = useState("");
+  const [loanAmount, setLoanAmount] = useState("");
+  const [loanPurpose, setLoanPurpose] = useState("");
+  const [currency, setCurrency] = useState("NPR");
+
+  const dispatch = useDispatch();
+  const {
+    balance,
+    loan: currentLoan,
+    loanPurpose: currentLoanPurpose,
+    isLoading,
+  } = useSelector((store) => store.account);
+
+  function handleDeposit() {
+    if (!depositAmount) return;
+    dispatch(deposit(depositAmount, currency));
+    setDepositAmount("");
+    setCurrency("NPR");
+  }
+
+  function handleWithdrawal() {
+    if (!withdrawalAmount) return;
+    if (balance < withdrawalAmount) return;
+    dispatch(withdraw(withdrawalAmount));
+    setWithdrawalAmount("");
+  }
+
+  function handleRequestLoan() {
+    if (!loanAmount || !loanPurpose) return;
+    dispatch(requestLoan(loanAmount, loanPurpose));
+    setLoanAmount("");
+    setLoanPurpose("");
+  }
+
+  function handlePayLoan() {
+    if (balance < currentLoan) return;
+    dispatch(payLoan());
+  }
+
+  return (
+    <div>
+      <h2>Your account operations</h2>
+      <div className="inputs">
+        <div>
+          <label>Deposit</label>
+          <input
+            type="number"
+            value={depositAmount}
+            onChange={(e) => setDepositAmount(+e.target.value)}
+          />
+          <select
+            value={currency}
+            onChange={(e) => setCurrency(e.target.value)}
+          >
+            <option value="NPR">Nepali rupees</option>
+            <option value="USD">US Dollar</option>
+            <option value="EUR">Euro</option>
+            <option value="INR">Indian rupees</option>
+          </select>
+
+          <button onClick={handleDeposit} disabled={isLoading}>
+            {isLoading ? "Converting..." : `Deposit ${depositAmount}`}
+          </button>
+        </div>
+
+        <div>
+          <label>Withdraw</label>
+          <input
+            type="number"
+            value={withdrawalAmount}
+            onChange={(e) => setWithdrawalAmount(+e.target.value)}
+          />
+          <button onClick={handleWithdrawal}>
+            Withdraw {withdrawalAmount}
+          </button>
+        </div>
+
+        <div>
+          <label>Request loan</label>
+          <input
+            type="number"
+            value={loanAmount}
+            onChange={(e) => setLoanAmount(+e.target.value)}
+            placeholder="Loan amount"
+          />
+          <input
+            value={loanPurpose}
+            onChange={(e) => setLoanPurpose(e.target.value)}
+            placeholder="Loan purpose"
+          />
+          <button onClick={handleRequestLoan}>Request loan</button>
+        </div>
+        {currentLoan > 0 && (
+          <div>
+            <span>
+              Pay back ${currentLoan}({currentLoanPurpose})
+            </span>
+            <button onClick={handlePayLoan}>Pay loan</button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default AccountOperations;
