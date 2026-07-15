f7ad165c74f3c71e384b93402127986f0c563127
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-07-15 15:43:22 +0545

    handling error

---
+++ 10-FAST-REACT-PIZZA/src/ui/AppLayout.jsx
@@ -0,0 +1,20 @@
+import { Outlet, useNavigation } from "react-router-dom"
+import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+import Loader from "./Loader";
+function AppLayOut() {
+    const navigation = useNavigation();
+    const isLoading = navigation.state === "loading";
+    return (
+        <div className="layout">
+            {isLoading && <Loader/>}
+            <Header />
+            <main>
+                <Outlet />
+            </main>
+            <CartOverview />
+        </div>
+    )
+}
+
+export default AppLayOut
