bf926a2f019e45452e4d018ab19b641469fa237b
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-07-18 19:06:16 +0545

    styling using tailwind css

--- 10-FAST-REACT-PIZZA/src/ui/Header.jsx
+++ 10-FAST-REACT-PIZZA/src/ui/Header.jsx
@@ -1,13 +1,16 @@
-import { Link } from "react-router-dom"
-import SearchOrder from "./SearchOrder"
+import { Link } from 'react-router-dom';
+import SearchOrder from './SearchOrder';
+import UserName from '../features/user/UserName';
 function Header() {
-    return (
-        <header>
-            <Link to="/">Fast React PIzza Co.</Link>
-            <SearchOrder />
-            <p>Prahans</p>
-        </header>
-    )
+  return (
+    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-widest">
+        Fast React PIzza Co.
+      </Link>
+      <SearchOrder />
+      <UserName />
+    </header>
+  );
 }
 
-export default Header
+export default Header;
