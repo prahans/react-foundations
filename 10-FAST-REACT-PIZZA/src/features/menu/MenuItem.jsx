44d3fccf5bc1ec2d110bac9251c00409bb83012d
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-07-19 10:14:59 +0545

    building the cart page

--- 10-FAST-REACT-PIZZA/src/features/menu/MenuItem.jsx
+++ 10-FAST-REACT-PIZZA/src/features/menu/MenuItem.jsx
@@ -1,7 +1,21 @@
 import { formatCurrency } from '../../utils/helpers';
 import Button from '../../ui/Button';
+import { useDispatch } from 'react-redux';
+import { addItem } from '../cart/cartSlice';
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const dispatch = useDispatch();
+
+  function handleAddToCard() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(newItem));
+  }
 
   return (
     <li className="flex gap-4 py-2">
@@ -23,7 +37,11 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-          <Button type="small">ADD TO CART</Button>
+          {!soldOut && (
+            <Button type="small" onClick={handleAddToCard}>
+              ADD TO CART
+            </Button>
+          )}
         </div>
       </div>
     </li>
--- 10-FAST-REACT-PIZZA/src/features/order/CreateOrder.jsx
+++ 10-FAST-REACT-PIZZA/src/features/order/CreateOrder.jsx
@@ -1,6 +1,7 @@
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../service/apiRestaurant';
 import Button from '../../ui/Button';
+import { useSelector } from 'react-redux';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -38,6 +39,7 @@ function CreateOrder() {
   const formErrors = useActionData();
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
+  const userName = useSelector((state) => state.user.userName);
 
   return (
     <div className="px-4 py-6">
@@ -46,7 +48,13 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input type="text" name="customer" required className="input grow" />
+          <input
+            type="text"
+            name="customer"
+            required
+            className="input grow"
+            defaultValue={userName}
+          />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
