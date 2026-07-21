2e5e970fc7fa070f4633ad114d7bd12d70bf7f7f
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-07-21 20:48:56 +0545

    Updating Data Without Navigation

--- 10-FAST-REACT-PIZZA/src/features/order/Order.jsx
+++ 10-FAST-REACT-PIZZA/src/features/order/Order.jsx
@@ -6,10 +6,20 @@ import {
   formatDate,
 } from '../../utils/helpers';
 import { getOrder } from '../../service/apiRestaurant';
-import { useLoaderData } from 'react-router-dom';
+import { useFetcher, useLoaderData } from 'react-router-dom';
+import { useEffect } from 'react';
+import UpdateOrder from './UpdateOrder';
 
 function Order() {
   const order = useLoaderData();
+  const fetcher = useFetcher();
+
+  useEffect(
+    function () {
+      if (!fetcher.data && fetcher.state === 'idle') fetcher.load('/menu');
+    },
+    [fetcher],
+  );
 
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
@@ -52,8 +62,16 @@ function Order() {
       </div>
 
       <ul className="divide-y divide-stone-200 border-b border-t">
-        {cart.map((item, i) => (
-          <OrderItem item={item} key={i} />
+        {cart.map((item) => (
+          <OrderItem
+            item={item}
+            key={item.pizzaId}
+            isLoadingIngredients={fetcher.state === 'loading'}
+            ingredients={
+              fetcher?.data?.find((el) => el.id === item.pizzaId)
+                ?.ingredients ?? []
+            }
+          />
         ))}
       </ul>
 
@@ -70,6 +88,7 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+      {!priority && <UpdateOrder />}
     </div>
   );
 }
