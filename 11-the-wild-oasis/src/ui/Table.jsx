d4dc01b8e0ace077b66bf8d135d2738c9b918a54
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-07-27 15:59:29 +0545

    Applying the Render Props Pattern

--- 11-the-wild-oasis/src/ui/Table.jsx
+++ 11-the-wild-oasis/src/ui/Table.jsx
@@ -85,7 +85,10 @@ function Row({ children }) {
     </StyledRow>
   );
 }
-function Body() {}
+function Body({ data, render }) {
+  if (!data.length) return <Empty>No data to show at the moment</Empty>;
+  return <StyledBody>{data.map(render)}</StyledBody>;
+}
 
 Table.Header = Header;
 Table.Row = Row;
