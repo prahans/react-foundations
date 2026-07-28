857e3bd3bd0a4e8ffe00a74deeec5e97ef6d2e8d
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-07-28 06:47:35 +0545

    Client-Side Filtering: Filtering Cabins

--- 11-the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ 11-the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -1,25 +1,9 @@
-import styled from "styled-components";
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import { useCabin } from "./useCabin";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import { useSearchParams } from "react-router-dom";
-
-const TableHeader = styled.header`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-
-  background-color: var(--color-grey-50);
-  border-bottom: 1px solid var(--color-grey-100);
-  text-transform: uppercase;
-  letter-spacing: 0.4px;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  padding: 1.6rem 2.4rem;
-`;
 
 function CabinTable() {
   const { cabins, isLoading } = useCabin();
@@ -32,7 +16,7 @@ function CabinTable() {
   let filterCabins;
   if (filterValue === "all") filterCabins = cabins;
   if (filterValue === "no-discount")
-    filterCabins = cabins.filter((cabin) => cabin.discount !== 0);
+    filterCabins = cabins.filter((cabin) => cabin.discount === 0);
   if (filterValue === "with-discount")
     filterCabins = cabins.filter((cabin) => cabin.discount > 0);
   return (
