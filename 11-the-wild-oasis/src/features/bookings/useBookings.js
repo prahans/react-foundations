2d755098d21190da50be5075da53d44a35874e88
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-07-28 13:47:00 +0545

    API-Side Sorting: Sorting Bookings

--- 11-the-wild-oasis/src/features/bookings/useBookings.js
+++ 11-the-wild-oasis/src/features/bookings/useBookings.js
@@ -11,13 +11,19 @@ export function useBookings() {
     !filterValue || filterValue === "all"
       ? null
       : { field: "status", value: filterValue };
+
+  // sort
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+
   const {
     data: bookings,
     isLoading,
     error,
   } = useQuery({
-    queryKey: ["bookings", filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { bookings, isLoading, error };
