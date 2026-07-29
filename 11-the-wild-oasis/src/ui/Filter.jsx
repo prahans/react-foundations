8046b5521932232437241d7697ec1b58dc8f98b7
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-07-29 10:04:04 +0545

    fixing pagination prob.(resetting page to 1 when ever filter is changed)

--- 11-the-wild-oasis/src/ui/Filter.jsx
+++ 11-the-wild-oasis/src/ui/Filter.jsx
@@ -39,9 +39,21 @@ function Filter({ filterField, options }) {
   const [searchParams, setSearchParams] = useSearchParams();
 
   const currentFilter = searchParams.get(filterField) || options.at(0).value;
+  // function handleClick(value) {
+  //   searchParams.set(filterField, value);
+  //   setSearchParams(searchParams);
+  // }
+
   function handleClick(value) {
-    searchParams.set(filterField, value);
-    setSearchParams(searchParams);
+    const newSearchParams = new URLSearchParams(searchParams);
+
+    newSearchParams.set(filterField, value);
+
+    if (searchParams.has("page")) {
+      newSearchParams.set("page", "1");
+    }
+
+    setSearchParams(newSearchParams);
   }
 
   return (
