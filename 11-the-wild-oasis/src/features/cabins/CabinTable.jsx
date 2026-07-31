254aecaa1500f7e88415382467a81b6e77bc2732
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-07-31 11:21:51 +0545

    Displaying a Line Chart With the Recharts Library

--- 11-the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ 11-the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -1,13 +1,13 @@
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
-import { useCabin } from "./useCabin";
+import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import { useSearchParams } from "react-router-dom";
 import Empty from "../../ui/Empty";
 
 function CabinTable() {
-  const { cabins, isLoading } = useCabin();
+  const { cabins, isLoading } = useCabins();
   const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
