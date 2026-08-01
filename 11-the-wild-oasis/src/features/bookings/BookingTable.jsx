aba1b7e60acc60a6e3fe8159208fd077e52f5927
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-08-01 19:04:32 +0545

    adding button in booking page to create new booking

--- 11-the-wild-oasis/src/features/bookings/BookingTable.jsx
+++ 11-the-wild-oasis/src/features/bookings/BookingTable.jsx
@@ -5,6 +5,9 @@ import { useBookings } from "./useBookings";
 import Spinner from "../../ui/Spinner";
 import BookingRow from "./BookingRow";
 import Pagination from "../../ui/Pagination";
+import Button from "../../ui/Button";
+import Modal from "../../ui/Modal";
+import CreateCabinForm from "../cabins/CreateCabinForm";
 
 function BookingTable() {
   const { bookings, isLoading, count } = useBookings();
@@ -13,28 +16,42 @@ function BookingTable() {
   if (!bookings.length) return <Empty resourceName="bookings" />;
 
   return (
-    <Menus>
-      <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
-        <Table.Header>
-          <div>Cabin</div>
-          <div>Guest</div>
-          <div>Dates</div>
-          <div>Status</div>
-          <div>Amount</div>
-          <div></div>
-        </Table.Header>
+    <>
+      <Menus>
+        <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
+          <Table.Header>
+            <div>Cabin</div>
+            <div>Guest</div>
+            <div>Dates</div>
+            <div>Status</div>
+            <div>Amount</div>
+            <div></div>
+          </Table.Header>
 
-        <Table.Body
-          data={bookings}
-          render={(booking) => (
-            <BookingRow key={booking.id} booking={booking} />
-          )}
-        />
-        <Table.Footer>
-          <Pagination count={count} />
-        </Table.Footer>
-      </Table>
-    </Menus>
+          <Table.Body
+            data={bookings}
+            render={(booking) => (
+              <BookingRow key={booking.id} booking={booking} />
+            )}
+          />
+          <Table.Footer>
+            <Pagination count={count} />
+          </Table.Footer>
+        </Table>
+        <div>
+          <div>
+            <Modal>
+              <Modal.Open opens="booking-form">
+                <Button>Add new booking</Button>
+              </Modal.Open>
+              <Modal.Window name="booking-form">
+                <CreateCabinForm />
+              </Modal.Window>
+            </Modal>
+          </div>
+        </div>
+      </Menus>
+    </>
   );
 }
 
